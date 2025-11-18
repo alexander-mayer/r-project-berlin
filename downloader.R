73f969d54da7c89497e3191fd2fb9762ed5625ec
alexander-mayer <166533315+alexander-mayer@users.noreply.github.com>
2025-11-18 17:07:19 +0100

    Download script outline
    First links

---
+++ downloader.R
@@ -0,0 +1,25 @@
+#we use this script to download our WFS data
+#possibly convert it as well?
+
+
+#https://tutorials.inbo.be/tutorials/spatial_wfs_services/
+
+
+
+#-----------------------------
+#DATA
+#-----------------------------
+#1 - Health and Social Factors
+#https://daten.berlin.de/datensaetze/gesundheits-und-sozialstrukturatlas-gesundheits-und-sozialindex-2022-gesix-wms-44ab83fa
+
+
+
+
+#2 - Air Pollution
+#https://daten.berlin.de/datensaetze/durchschnittliche-jahrliche-luftschadstoffbelastung-modelldaten-umweltatlas-wms-d38aed69
+
+
+
+
+#3 - Noise Pollution
+#https://daten.berlin.de/datensaetze/umweltgerechtigkeit-2021-2022-umweltatlas-wms-87f1aaf7
