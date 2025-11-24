2ca850a939715ad19d419fa1ac60e75dd547ef56
alexander-mayer <166533315+alexander-mayer@users.noreply.github.com>
2025-11-24 16:20:02 +0100

    Downloader Update
    WMS image request now works

--- downloader.R
+++ downloader.R
@@ -50,6 +50,9 @@ layers_noise <- c("b_09_01_1UGlaerm2021", #Kernindikator Lärmbelastung
 #WMS is online and can break, we want offline files
 
 
+#Example WMS-Request, we try to get a PNG and probably need to georeference it again
+#https://gdi.berlin.de/services/wms/ua_luftschadstoffbelastung?service=WMS&version=1.3.0&request=GetMap&layers=a_pollutant_grid_avg_no2_2024&styles=&crs=EPSG:32633&bbox=369950,5799450,415850,5837300&width=500&height=500&format=image%2Fpng
+
 
 
 #-----------------------------
