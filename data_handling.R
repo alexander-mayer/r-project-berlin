3dee64cd8c5f01761c15c2110538e25d6fc50486
alexander-mayer <166533315+alexander-mayer@users.noreply.github.com>
2026-01-23 12:12:49 +0100

    All data rasterised

--- data_handling.R
+++ data_handling.R
@@ -36,11 +36,38 @@ gruen <- gruen %>% filter(unbewofl != "ja" | is.na(unbewofl))
 bio <- bio %>% filter(unbewofl != "ja" | is.na(unbewofl))
 sozial <- sozial %>% filter(unbewofl != "ja" | is.na(unbewofl))
 
-laerm %>%
+#For finding out the categories
+sozial %>%
   st_drop_geometry() %>%
   distinct(ekategorie)
 
 #Clean up data
+#TODO
+
+#Next step: Encoding as numerical data
+bio$ekategorie_num <- dplyr::recode(
+  bio$ekategorie,
+  "NA" = 0,
+  "low"   = 1,
+  "medium" = 2,
+  "high"   = 3
+)
+
+gruen$ekategorie_num <- dplyr::recode(
+  gruen$ekategorie,
+  "NA" = 0,
+  "poor"   = 1,
+  "medium" = 2,
+  "good"   = 3
+)
+
+gssa$ekategorie_num <- dplyr::recode(
+  gssa$ekategorie,
+  "NA" = 0,
+  "low"   = 1,
+  "medium" = 2,
+  "high"   = 3
+)
 
 laerm$ekategorie_num <- dplyr::recode(
   laerm$ekategorie,
@@ -50,7 +77,28 @@ laerm$ekategorie_num <- dplyr::recode(
   "high"   = 3
 )
 
+luft$ekategorie_num <- dplyr::recode(
+  luft$ekategorie,
+  "NA" = 0,
+  "low"   = 1,
+  "medium" = 2,
+  "high"   = 3
+)
+
+sozial$ekategorie_num <- dplyr::recode(
+  sozial$ekategorie,
+  "NA" = 0,
+  "low, very low Status index"   = 1,
+  "mean Status index" = 2,
+  "high Status index"   = 3
+)
+
+bio_v <- vect(bio)
+gruen_v <- vect(gruen)
+gssa_v <- vect(gssa)
 laerm_v <- vect(laerm)
+luft_v <- vect(luft)
+sozial_v <- vect(sozial)
 
 #Use this template for all conversions
 #We set the raster size for all layers here!
@@ -60,6 +108,62 @@ r_template <- rast(
   crs = target_crs
 )
 
+#Rasterize and assign the categorical values back
+#bio#######################
+bio_raster <- rasterize(
+  bio_v,
+  r_template,
+  field = "ekategorie_num"
+)
+
+levels(bio_raster) <- data.frame(
+  value = c(1, 2, 3),
+  label = c("low", "medium", "high")
+)
+
+writeRaster(
+  bio_raster,
+  "data/R_bio.tif",
+  overwrite = TRUE
+)
+
+#gruen#######################
+gruen_raster <- rasterize(
+  gruen_v,
+  r_template,
+  field = "ekategorie_num"
+)
+
+levels(gruen_raster) <- data.frame(
+  value = c(1, 2, 3),
+  label = c("low", "medium", "high")
+)
+
+writeRaster(
+  gruen_raster,
+  "data/R_gruen.tif",
+  overwrite = TRUE
+)
+
+#gssa#######################
+gssa_raster <- rasterize(
+  gssa_v,
+  r_template,
+  field = "ekategorie_num"
+)
+
+levels(gssa_raster) <- data.frame(
+  value = c(1, 2, 3),
+  label = c("low", "medium", "high")
+)
+
+writeRaster(
+  gssa_raster,
+  "data/R_gssa.tif",
+  overwrite = TRUE
+)
+
+#laerm#######################
 laerm_raster <- rasterize(
   laerm_v,
   r_template,
@@ -74,6 +178,42 @@ levels(laerm_raster) <- data.frame(
 writeRaster(
   laerm_raster,
   "data/laerm.tif",
+  overwrite = TRUE
+)
+
+#luft#######################
+luft_raster <- rasterize(
+  luft_v,
+  r_template,
+  field = "ekategorie_num"
+)
+
+levels(luft_raster) <- data.frame(
+  value = c(1, 2, 3),
+  label = c("low", "medium", "high")
+)
+
+writeRaster(
+  luft_raster,
+  "data/R_luft.tif",
+  overwrite = TRUE
+)
+
+#sozial#######################
+sozial_raster <- rasterize(
+  sozial_v,
+  r_template,
+  field = "ekategorie_num"
+)
+
+levels(sozial_raster) <- data.frame(
+  value = c(1, 2, 3),
+  label = c("low", "medium", "high")
+)
+
+writeRaster(
+  sozial_raster,
+  "data/R_sozial.tif",
   overwrite = TRUE
 )
 
